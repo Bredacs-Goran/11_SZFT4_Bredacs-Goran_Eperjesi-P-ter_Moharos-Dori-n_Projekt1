d82065547338d80c7b0ac5db18beebaf29c8e641
Bredács Goran <93593050+Bredacs-Goran@users.noreply.github.com>
2024-05-21 11:04:09 +0200

    Game base
    Játék alap kicsomagolva

---
+++ Jatek/Fojatek/Program.cs
@@ -0,0 +1,8 @@
+﻿using Jatek_Lib;
+
+Console.WriteLine("Köszöntünk a Dungeon-Rush játékban!");
+Console.WriteLine();
+Console.Write("Kérem adja meg a főhős nevét: ");
+string fohosnev = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine();
