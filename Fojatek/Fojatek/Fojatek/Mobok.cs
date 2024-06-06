506c21c11f7f5a9b206e56aa204f859947ce376e
Bredács Goran <93593050+Bredacs-Goran@users.noreply.github.com>
2024-06-06 16:52:09 +0200

    Final game
    A kész játék

---
+++ Fojatek/Fojatek/Fojatek/Mobok.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fojatek
+{
+    internal class Mobok
+    {
+        private double slimehp;
+        public double Slimehp { get; set; }
+
+        private double traphp;
+        public double Traphp { get; set; }
+
+        private double imphp;
+        public double Impphp { get; set; }
+        private double orchp;
+        public double Orchp { get; set; }
+        private double bwhp;
+        public double Bwhp { get; set; }
+    }
+}
